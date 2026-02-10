fa12b073a63f956db106ee4e512f9840f9842f31
Jaime <jaimeespinalpr@gmail.com>
2026-02-09 22:29:17 -0500

    Remove legacy PHP API files

--- api/health.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__ . '/db.php';
-$pdo->query('SELECT 1');
-echo json_encode(['ok' => true]);
--- api/ping.php
+++
@@ -1,3 +0,0 @@
-<?php
-require __DIR__ . '/security.php';
-echo json_encode(['ok' => true, 'session_active' => session_status() === PHP_SESSION_ACTIVE]);
